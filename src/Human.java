5bd151ba8dbc707241c6f972ebf9b0d221acefad
Samsung <arslan.zakirov.1996@mail.ru>
2022-11-14 18:27:12 +0300

    Домашнее задание по ООП №1(1-2) с использованием конструктора

--- src/Human.java
+++ src/Human.java
@@ -1,13 +1,19 @@
 public class Human {
-    public int yearOfBirth;
+
     public String name;
     public String town;
+    public int yearOfBirth;
     public String jobTitle;
 
-
+    public Human( String name, String town, int yearOfBirth, String jobTitle) {
+        this.name = name;
+        this.town = town;
+        this.yearOfBirth = yearOfBirth;
+        this.jobTitle = jobTitle;
+    }
 
     void say() {
-        System.out.println("Привет! Меня зовут "+name+".Я из города-"+town+".Я родился в "+yearOfBirth+"году.Я работаю на должности "+ jobTitle+ ". Будем знакомы!");
+        System.out.println("Привет! Меня зовут "+name+".Я из города-"+town+".Я родился в "+yearOfBirth+" году.Я работаю на должности "+ jobTitle+ ".Будем знакомы!");
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -1,32 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Human maxim =new Human();
-        maxim.name = "Максим";
-        maxim.town = "Минск";
-        maxim.yearOfBirth = 1988;
-        maxim.jobTitle = "бренд-менеджер";
+        Human maxim =new Human("Максим","Минск",1988,"бренд-менеджер");
+        Human anna = new Human("Аня","Москва",1993,"методист образовательных программ");
+        Human kate = new Human("Катя","Калиниград",1992,"продакт-менеджер");
+        Human artem = new Human("Артем","Москва",1995,"директор по развитию");
         maxim.say();
-
-        Human anna = new Human();
-        anna.name = "Аня";
-        anna.town = "Москва";
-        anna.yearOfBirth = 1993;
-        anna.jobTitle = "методист образовательных программ";
         anna.say();
-
-        Human kate = new Human();
-        kate.name = "Катя";
-        kate.town = "Калинград";
-        kate.yearOfBirth = 1992;
-        kate.jobTitle = "продакт-менеджер";
         kate.say();
-
-        Human artem = new Human();
-        artem.name = "Артем";
-        artem.town = "Москва";
-        artem.yearOfBirth = 1995;
-        artem.jobTitle = "директор по развитию бизнеса";
         artem.say();
-
     }
 }
