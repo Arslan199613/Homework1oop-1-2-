bf70cf57c09a02876b7803d1f8fcc52887634ee2
Samsung <arslan.zakirov.1996@mail.ru>
2022-11-14 20:57:20 +0300

    Домашнее задание по ООП №1(1-2) с использованием конструктора+проверка

--- src/Human.java
+++ src/Human.java
@@ -6,12 +6,27 @@ public class Human {
     public String jobTitle;
 
     public Human( String name, String town, int yearOfBirth, String jobTitle) {
-        this.name = name;
-        this.town = town;
-        this.yearOfBirth = yearOfBirth;
-        this.jobTitle = jobTitle;
+        if (name == null) {
+            this.name = "Информация не указана";
+        }else{
+            this.name = name;
+        }
+        if (town == null) {
+            this.town = "Информация не указана";
+        }else{
+            this.town = town;
+        }
+        if (yearOfBirth < 0) {
+            this.yearOfBirth = 0;
+        }else{
+            this.yearOfBirth = yearOfBirth;
+        }
+        if (jobTitle == null) {
+            this.jobTitle = "Информация не указана";
+        }else {
+            this.jobTitle = jobTitle;
+        }
     }
-
     void say() {
         System.out.println("Привет! Меня зовут "+name+".Я из города-"+town+".Я родился в "+yearOfBirth+" году.Я работаю на должности "+ jobTitle+ ".Будем знакомы!");
     }
