5220e9d9c9dc4063ef06fe8ed03cef7015c289e7
Samsung <arslan.zakirov.1996@mail.ru>
2022-11-14 16:27:28 +0300

    Домашнее задание по ООП №1(1-2)

--- src/Human.java
+++ src/Human.java
@@ -1,1 +1,16 @@
+public class Human {
+    public int yearOfBirth;
+    public String name;
+    public String town;
+    public String jobTitle;
+
+
+
+    void say() {
+        System.out.println("Привет! Меня зовут "+name+".Я из города-"+town+".Я родился в "+yearOfBirth+"году.Я работаю на должности "+ jobTitle+ ". Будем знакомы!");
+    }
+}
+
+
+
 
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,32 @@
 public class Main {
     public static void main(String[] args) {
+        Human maxim =new Human();
+        maxim.name = "Максим";
+        maxim.town = "Минск";
+        maxim.yearOfBirth = 1988;
+        maxim.jobTitle = "бренд-менеджер";
+        maxim.say();
+
+        Human anna = new Human();
+        anna.name = "Аня";
+        anna.town = "Москва";
+        anna.yearOfBirth = 1993;
+        anna.jobTitle = "методист образовательных программ";
+        anna.say();
+
+        Human kate = new Human();
+        kate.name = "Катя";
+        kate.town = "Калинград";
+        kate.yearOfBirth = 1992;
+        kate.jobTitle = "продакт-менеджер";
+        kate.say();
+
+        Human artem = new Human();
+        artem.name = "Артем";
+        artem.town = "Москва";
+        artem.yearOfBirth = 1995;
+        artem.jobTitle = "директор по развитию бизнеса";
+        artem.say();
 
     }
 }
